13198688e7b350f3e3e5574d0730409150e821c3
makar <pedromakardomingos@gmail.com>
2025-09-05 09:28:29 -0300

    2

--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -2,9 +2,10 @@
 const http = require('http');
 
 //criando o servidor
-const servidor = http.createServer((req, res) => {
+http.createServer((req, res) => {
     //definindo o cabeçalho da resposta
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     //enviando a resposta
-    res.end('Olá, mundo! Este é meu primeiro servidor HTTP em Node.js\n');
-});listen(3000); 
+    res.end('Olá, mundo!');
+}).listen(8080); 
+console.log('Servidor rodando em http://localhost:8080/');
